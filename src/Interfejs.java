bff107d5297de295a17882cf5ca2a1f1711e970d
Natalia Nazaruk <nazarjazz@wp.pl>
2017-10-01 22:33:53 +0200

    Added pattern to specify date and wage of the cat

--- src/Interfejs.java
+++ src/Interfejs.java
@@ -1,6 +1,7 @@
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Interfejs {
 
@@ -15,22 +16,29 @@ public class Interfejs {
         System.out.print("Podaj, kto jest opiekunem kota: ");
         kot.setImieOpiekuna(getUserInput());
 
+        Pattern wzorzecDaty = Pattern.compile("[0-9]{4}.[0-1]?[0-9].[0-3]?[0-9]");
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd");
+        String dataUrodzeniaWczytana;
         do {
             System.out.print("Podaj datę urodzenia kota w formacie RRRR.MM.DD: ");
-            try {
-                kot.setUrodzenie(sdf.parse(getUserInput()));
-            } catch (ParseException pe) {
-                System.out.println("Coś jest nie tak z formatem daty! Przykładowa data: 2014.01.05");
+            dataUrodzeniaWczytana = getUserInput();
+            if (wzorzecDaty.matcher(dataUrodzeniaWczytana).matches()) {
+                try {
+                    kot.setUrodzenie(sdf.parse(dataUrodzeniaWczytana));
+                } catch (ParseException pe) {
+                    System.out.println("Coś jest nie tak z datą! Przykładowa data: 2014.01.05");
+                }
             }
-        } while (kot.getUrodzenie() == null);
+        } while (kot.getUrodzenie()==null);
 
+        Pattern wzorzecWagi = Pattern.compile("[0-9]+(\\.[0-9]+)?");
+        String wagaWczytana;
         do {
             System.out.print("Podaj wagę kota: ");
-            try {
-                kot.setWaga(Float.valueOf(getUserInput()));
-            } catch (NumberFormatException nfe) {
-                System.out.println("Coś jest nie tak z formatem wagi! Przykładowa waga: 10.0");
+            wagaWczytana = getUserInput();
+
+            if (wzorzecWagi.matcher(wagaWczytana).matches()) {
+                kot.setWaga(Float.valueOf(wagaWczytana));
             }
         } while (kot.getWaga() == null);
 
